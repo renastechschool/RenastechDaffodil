e499bae9fed4cf58f88cf8ebc7b6a99bfb9bd3b7
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-23 16:59:02 -0500

    update

---
+++ 02_JAVA/src/student/Omer/lab01.java
@@ -0,0 +1,10 @@
+package student.Omer;
+
+public class lab01 {
+    public static void main(String[] args) {
+        String str1= "OMER CIHAD BORKLU";
+        System.out.println(str1);
+    }
+
+
+}
