055f2bb0c97f05e0b8e319806a7b2171f989b6b2
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-25 22:13:47 -0500

    updated

--- 02_JAVA/src/student/Omer/lab01.java
+++ 02_JAVA/src/student/Omer/lab01.java
@@ -2,7 +2,7 @@ package student.Omer;
 
 public class lab01 {
     public static void main(String[] args) {
-        String str1= "OMER CIHAD BORKLU";
+        String str1= "OMER CIHAD BORKLU 01";
         System.out.println(str1);
     }
 
