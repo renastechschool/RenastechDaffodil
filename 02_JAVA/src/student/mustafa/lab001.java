e1f36191be77491d9df21753b1857e9745644496
mustafaubur <uburmustafa@gmail.com>
2022-05-01 15:11:33 -0700

    mustafa

--- 02_JAVA/src/student/mustafa/lab001.java
+++ 02_JAVA/src/student/mustafa/lab001.java
@@ -2,6 +2,17 @@ package student.mustafa;
 
 public class lab001 {
     public static void main(String[] args) {
+        //Write a return method that can reverse  String
+        //Ex: Reverse("ABCD"); ==> DCBA
 
+         String name="ABCD";
+       int lastindexofName=name.length()-1;
+       String reverseword="";
+        for (int i = lastindexofName; i >=0 ; i--) {
+            reverseword+=name.charAt(i);
+            System.out.println(reverseword);
+        }
+        System.out.println("******************");
+        System.out.println(reverseword);
     }
 }
