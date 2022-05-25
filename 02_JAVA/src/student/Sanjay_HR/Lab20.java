88b0f7d59f6c8d7f801d81d2db070e2133e775e4
Sanjay <sanjayhramnarine@gmail.com>
2022-05-25 19:16:03 -0400

    update

--- 02_JAVA/src/student/Sanjay_HR/Lab20.java
+++ 02_JAVA/src/student/Sanjay_HR/Lab20.java
@@ -2,5 +2,14 @@ package student.Sanjay_HR;
 
 public class Lab20 {
     // Swap two variable' values without using a third variable
-
+    public static void main(String[] args) {
+        swapTwoNumbers(1,2);
+    }
+    public static void swapTwoNumbers(int a, int b){
+        System.out.println("Before swap: Number1= "+a+", Number2= "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swap: Number1= "+a+", Number2= "+b);
+    }
 }
