0b4e9810235ce2be9375b10cbcdc689b2d59f72e
desto <dbatgi01@gmail.com>
2022-07-31 20:09:50 -0400

    Update

--- 03_SeleniumTestNG/src/test/java/students/Destyar/lab12.java
+++ 03_SeleniumTestNG/src/test/java/students/Destyar/lab12.java
@@ -1,5 +1,19 @@
 package students.Destyar;
 
+import code.utilities.WebDriverUtil;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class lab12 {
 
     //Navigate to "https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt"
@@ -12,4 +26,65 @@ public class lab12 {
 //Enter your credentials
 //Enter the show password button
 //Switch to first tab
+
+
+
+    WebDriver driver;
+    String nameForAlert;
+
+
+
+    @Test
+    public void TC1() {
+
+        driver = WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.navigate().to("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt");
+        driver.manage().deleteAllCookies();
+        driver.switchTo().frame(1);
+        driver.findElement(By.cssSelector("button[onclick='myFunction()']")).click();
+
+        Alert alert = driver.switchTo().alert();
+        String expectedText = "Kiyan";
+        alert.sendKeys(expectedText);
+        alert.accept();
+
+        Assert.assertTrue(driver.findElement(By.id("demo")).getText().contains(expectedText));
+
+
+        driver.switchTo().defaultContent();
+        WebElement homepage = driver.findElement(By.id("tryhome"));
+        homepage.click();
+
+        Set<String> AllWindowHandles = driver.getWindowHandles();
+        List<String> WindowsList = new ArrayList<>(AllWindowHandles);
+        driver.switchTo().window(WindowsList.get(1));
+        System.out.println("Window handle is : " + driver.getWindowHandle());
+
+        driver.findElement(By.id("w3loginbtn")).click();
+        driver.findElement(By.id("modalusername")).sendKeys("Kiyan213@gmail.com");
+        driver.findElement(By.id("current-password")).sendKeys("392739");
+        driver.findElement(By.cssSelector("span[class='PasswordInput_show_pwd_btn__Ncc9S']")).click();
+
+        Set<String> MainWindow=driver.getWindowHandles();
+        Iterator<String > itr=MainWindow.iterator();
+        String parentWindow= itr.next();
+        driver.switchTo().window(parentWindow);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
